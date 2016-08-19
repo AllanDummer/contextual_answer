36e317bf9b72d4b1a7923785e9884f850cd60479
Allan Dummer <allan@dummer.co.za>
2016-08-19 11:35:44 +0200

    Update contextual-answer-score.tpl.php
    Fixed coding standards

--- theme/contextual-answer-score.tpl.php
+++ theme/contextual-answer-score.tpl.php
@@ -1,9 +1,9 @@
 <div class="quiz-report-score-container <?php print $class ?>">
-  <span>
+	<span>
     <?php
     $perc = round(($score / $max_score) * 100, 0);
     $percentage = ' - (' . $perc . '%)';
     ?>
-    <?php print t('Score:') ?> <?php print $score ?> <?php print t('answers correct out of a possible') . ' ' . $max_score ?><?php print $percentage ?>
+    <?php print t('Score:') ?> <?php print $score ?> <?php print t('answers correct out of a possible') . ' ' . $max_score ?><?php print $percentage?>
   </span>
 </div>
